ff162f5dea05b3b8df510797a3cff9f1b959c0c9
Thomas Fiddian-Green <TFiddian-Green@bayviewglen.ca>
2022-09-08 14:47:54 -0400

    Initial Commit

---
+++ UNIT 1 - INTRODUCTION TO JAVA/src/day2/ExampleOne.java
@@ -0,0 +1,28 @@
+package day2;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+/**
+ * This is a block comment that spans more than one line (usually)
+ * 
+ * The class is called ExampleOne (same name as the file)
+ * One of the classes in the project must have a main method (function) that tells java where to start running
+ */
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AP Computer Science");
+
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+
+
+        //println adds a new line after printing 
+        //print leaves the cursor where it was after it prints
+        System.out.print("D");
+        System.out.print("E");
+        System.out.print("F");
+    }
+    
+}
