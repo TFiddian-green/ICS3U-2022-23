512d75b97f5844d222e5e9cb5a62d8350102882e
Thomas Fiddian-Green <TFiddian-Green@bayviewglen.ca>
2022-09-12 15:04:42 -0400

    Primitive Data Variables and Assignment

---
+++ UNIT 1 - INTRODUCTION TO JAVA/src/day3/ExampleOne.java
@@ -0,0 +1,24 @@
+package day3;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        // int -> integer (have no decimals)
+        // double -> (decimal numbers)
+        // boolean -> true / false
+
+        // naming convention for variables (camelCase)
+
+        // int age = 17.9; cannot convert a double to an int. 17.9 is a double and age
+        // is an int
+        int numberOfStudents = 13;      // = assignment operator 
+
+
+
+
+
+
+
+
+        // String concatenation is when I join something to a String
+    }
+}
